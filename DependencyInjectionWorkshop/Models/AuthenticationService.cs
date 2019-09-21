9026520e905c5f59ab48cab4c154b7b545136ba5
Mystic <mysteriousGE@gmail.com>
2019-09-21 19:54:24 +0800

    First version legacy code.

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,6 +1,49 @@
-﻿namespace DependencyInjectionWorkshop.Models
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Dapper;
+
+namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
+        public bool Verify(string accountId, string password, string otp)
+        {
+            string hashedPasswordFromDb;
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                hashedPasswordFromDb = connection.Query<string>("spGetUserPassword", new { Id = accountId },
+                    commandType: CommandType.StoredProcedure).SingleOrDefault();
+            }
+
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+            foreach (var theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            var hashedPassword = hash.ToString();
+
+            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
+            var response = httpClient.PostAsJsonAsync("api/otps", accountId).Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"web api error, accountId:{accountId}");
+            }
+            var currentOtp = response.Content.ReadAsAsync<string>().Result;
+
+            if (hashedPassword == hashedPasswordFromDb && otp == currentOtp)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
