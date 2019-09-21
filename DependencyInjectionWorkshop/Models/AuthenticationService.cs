83a85d1051787b9ba1b3327bec3eb103e7b7d850
Mystic <mysteriousGE@gmail.com>
2019-09-21 20:25:03 +0800

    Add notify().

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using Dapper;
+using SlackAPI;
 
 namespace DependencyInjectionWorkshop.Models
 {
@@ -42,6 +43,8 @@ namespace DependencyInjectionWorkshop.Models
             }
             else
             {
+                var slackClient = new SlackClient("my api token");
+                slackClient.PostMessage(response1 => { }, "my channel", accountId, "my bot name");
                 return false;
             }
         }
